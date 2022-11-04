eef202eeecf3ebf3dcf0353e6c6418dfa654b2d5
lumi <lucijamicic154@gmail.com>
2022-11-04 23:38:06 +0100

    vise manje sve

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
-const port = 3000
+const externalUrl = process.env.RENDER_EXTERNAL_URL
+const port = externalUrl && process.env.port ? parseInt(process.env.port) : 3000;
 
+require('dotenv').config();
+
+const { auth, requiresAuth } = require('express-openid-connect');
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: externalUrl || process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+  })
+);
+
+app.use( bodyParser.json() );      
+    app.use(bodyParser.urlencoded({    
+        extended: true
+    })
+)
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname+'public/css'))
 app.use('/js',express.static(__dirname+'public/js'))
@@ -13,6 +37,57 @@ app.get('',(req,res)=>{
     res.render('index')
 })
 
-app.listen(port,()=>{
-    console.info(`LIstening on port ${port}`)
+app.get('/sigurnixss',(req,res)=>{
+    res.render('sigurnixss')
 })
+
+app.get('/bac/sigurnibac/user',(req,res)=>{
+    res.render('sigurnibac')
+})
+
+app.get('/nesigurnixss',(req,res)=>{
+    
+    res.render('nesigurnixss')
+})
+
+app.get('/bac/nesigurnibac/user',(req,res)=>{
+    res.render('nesigurnibac')
+})
+
+app.get('/bac/sigurnibac/admin',requiresAuth(),(req,res)=>{
+    var userEmailJson = JSON.stringify(req.oidc.user)
+    const userEmailP = JSON.parse(userEmailJson)
+    const userEmail = userEmailP.email
+    res.render('admin2',{currentUser:userEmail})
+})
+
+app.post('/nesigurnixss',(req, res)=>{
+    const nezasticeniPodatak = String(req.body.nezasticeno)
+    const prezime = String(req.body.prezime)
+    res.render('rezultatNezasticeniXss',{podatak:nezasticeniPodatak,prezime:prezime})
+})
+
+app.get('/rezultat',(req,res)=>{
+    
+    res.render('rezultatZasticeniXss')
+})
+
+app.get('/bac/nesigurnibac/admin',(req,res)=>{
+    
+    res.render('admin1')
+})
+
+app.post('/sigurnixss',(req, res)=>{
+    const zasticeniPodatak = String(req.body.zasticeno)
+    const prezime = String(req.body.prezimez)
+    res.render('rezultatZasticeniXss',{podatak:zasticeniPodatak,prezime:prezime})
+})
+
+if(externalUrl){
+    const hostname= '127.0.0.1';
+    app.listen(port,hostname, ()=>{
+      console.log(`Server locally running at http://${hostname}:${port}/ and from outside on ${externalUrl}`)
+    })
+  }else{
+    app.listen(port,()=>console.info(`Listening on port ${port}`))
+  }
